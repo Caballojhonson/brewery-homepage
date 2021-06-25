527201bf1b23424ea246a54bfe5bacee53b96112
caballo <imanol.l.p@hotmail.com>
2021-06-25 12:30:22 +0200

    Refactored home section code, added progressive rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style.css';
 import logoSB from './images/logo.png'
+import githubPNG from './images/github-logo.png'
 
 const sideBar = (() => {
     const main = document.getElementById('content');
+    const scrollable = document.createElement('div');
+    scrollable.id = 'scrollable';
 
     const sideContainer = document.createElement('div');
     sideContainer.id = 'sideBar';
@@ -17,23 +20,35 @@ const sideBar = (() => {
     linkContainer.id = 'linkContainer';
     const homeBtn = document.createElement('h3');
     homeBtn.classList.add('navbar');
-    homeBtn.innerHTML = 'Home'
+    homeBtn.textContent = 'Home'
     const productBtn = document.createElement('h3');
     productBtn.classList.add('navbar');
+    productBtn.textContent = 'Products';
     const aboutBtn = document.createElement('h3');
     aboutBtn.classList.add('navbar');
+    aboutBtn.textContent = 'About us';
     const contactBtn = document.createElement('h3');
     contactBtn.classList.add('navbar');
+    contactBtn.textContent = 'Contact';
 
     const copyContainer = document.createElement('div');
     copyContainer.id = 'copyContainer';
-    const gitLogo = document.createElement('img');
+    const linkToGit = document.createElement('a');
+    linkToGit.id = 'linkToGit';
+    linkToGit.setAttribute('href', 'https://github.com/Caballojhonson')
+    linkToGit.setAttribute('target', '_blank')
+    const gitLogo = new Image();
+    gitLogo.src = githubPNG;
     gitLogo.id = 'gitLogo';
+
     const copyright = document.createElement('h6');
     copyright.id = 'copyright';
+    copyright.innerHTML = '\u00A9 2021, CaballoJhonson. All rights reserved.'
+
 
     const render = () => {
         main.appendChild(sideContainer);
+        main.appendChild(scrollable);
 
         sideContainer.appendChild(logoContainer);
         sideContainer.appendChild(linkContainer);
@@ -46,11 +61,46 @@ const sideBar = (() => {
         linkContainer.appendChild(aboutBtn);
         linkContainer.appendChild(contactBtn);
 
-        copyContainer.appendChild(gitLogo);
+        copyContainer.appendChild(linkToGit)
+        linkToGit.appendChild(gitLogo);
         copyContainer.appendChild(copyright);
+    };
+
+    return { scrollable, render }
+})();
+
+const home = (() => {
+    const main = sideBar.scrollable;
+
+    const titles = [
+        'The last brewery to be created in Barcelona before the 2020 pandemic',
+        'First ever "company" to avoid selling its own products',
+        'First modern brewery to never brew an IPA',
+        '"Short Brewing"',
+        'The first brewery to create its website after bankrupcy',
+    ];
+
+    const milestones = [
+        'January 2020',
+        'February 2020',
+
+    ];
+
+    const render = () => {
+        for (let i = 0; i < titles.length; i++) {
+            setTimeout(() => {
+                const title = document.createElement('h2');
+                title.classList.add('titles');
+                title.textContent = titles[i];
+                main.appendChild(title);
+            }, i * 1500);
         };
+    };
 
     return { render }
 })();
 
+
 sideBar.render();
+home.render();
+
