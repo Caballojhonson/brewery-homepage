666f7f35744e5837bee89c27bfd2a0e3cd6afb8b
caballo <imanol.l.p@hotmail.com>
2021-06-26 13:13:51 +0200

    Finished about tab

--- src/index.js
+++ src/index.js
@@ -257,18 +257,36 @@ const products = (() => {
     const main = sideBar.scrollable;
 
     const text = [
-        `Short Brewing is a non-existant (but legal) very-micro brewery.
+        `Short Brewing was created as a side project to fill the urge of acomplishing something after failing to open a restaurant.
+        It was created by two semi-alcoholic ex-associates in the middle of a belgian beer drinking session. 
+        <br><br><br>
+        We found the global craft beer scenario to be too repetitive, predictable and way too industrial. While IPAs were flooding the
+        market in all it's forms and variations, we thought there was a place for more traditional styles in the market. 
+        So we decided to give it a try, why not make a real craft beer? While others just punch a bunch of buttons in a touch screen 
+        on one end and output sweet wort on the other,
+        we wanted to make it for real. How can a company be shipping 100 Million bottles of beer a year and yet still
+        call it craft? We'll do it ALL by hand, we said...
+        <br> 
+        Let's short brew the market!
+        <br><br><br>
+        Short Brewing is (was) a non-existant (but legal) very-micro brewery. 
+        <br>
         All our products are painstakingly handmade. We bottle and cap manually. We label by hand, bottle per bottle. One by one.
-        Each cap is then hand stamped with lot info and production number with a manual wooden stamp, inked by hand, of course.
+        Each cap is then hand stamped with batch info and production number with a manual wooden stamp, inked by hand, of course.
+        The same guy making the recipes also makes all the trademark designs, social media, accounting, photography
+         and this stupid website.
+        <br><br><br>
         We like to be called gipsy brewers, although truth is we don't have the resources to own our own brewery. 
-        Being gipsys is one of the reasons for the downfall of our great company. Taxes, sales, infrastructure and lack of
+        Being gipsys is one of the reasons for the downfall of our great company. Problem is you don't choose when your next batch
+        is going to be brewed. Instead you are in hands of other people's schedules, providers and problems. Taxes, sales, infrastructure and lack of
         interest didn't help either. We are proud to have endured so long anyway. We have become experts in non-functional 
         projects that come to a halt before even being a thing. Praise the lord and GG Allin. Thanks for your overwhelming support.
-        (The logo on the center of the bottle are my balls painted in aquarella)`
+        <br><br><br>
+        (The logo on the center of the bottle are my aquarella painted balls. Long story...)`
     ];
     const render = () => {
     const mainText = document.createElement('h6');
-    mainText.textContent = text[0];
+    mainText.innerHTML = text[0];
     mainText.id = 'aboutText'
     main.appendChild(mainText);
     }
@@ -282,6 +300,7 @@ home.render();
 
 const handle = (() => {
     const background = document.getElementById('content');
+    const cursorIcon = document.getElementById('scrollSection')
     const homeBtn = document.getElementById('homeBtn');
     const productBtn = document.getElementById('productBtn');
     const aboutBtn = document.getElementById('aboutBtn');
@@ -300,23 +319,28 @@ const handle = (() => {
             sideBar.scrollable.innerHTML = '';
             background.style.backgroundImage = "var(--mainBackground)";
             underline(homeBtn);
+            cursorIcon.style.display = 'inherit';
             home.render();
         })
         productBtn.addEventListener('click', () => {
             sideBar.scrollable.innerHTML = '';
-            background.style.backgroundImage = "var(--bottleBackground)";
+            background.style.backgroundImage = "var(--productsBackground)";
             underline(productBtn);
+            cursorIcon.style.display = 'inherit';
             products.render();
         })
         aboutBtn.addEventListener('click', () => {
             sideBar.scrollable.innerHTML = '';
+            background.style.backgroundImage = "var(--aboutBackground)";
             underline(aboutBtn);
+            cursorIcon.style.display = 'inherit';
             about.render();
         })
         contactBtn.addEventListener('click', () => {
             sideBar.scrollable.innerHTML = '';
+            background.style.backgroundImage = "var(--contactBackground)";
             underline(contactBtn);
-
+            cursorIcon.style.display = 'none';
         })
     }
     return { listners }
